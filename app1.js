62ba1cf690b3858d00099e8d3035ddb55917c420
Matt Sander <msander@mintventures.net>
2020-06-15 12:42:18 -0400

    third commit

--- app1.js
+++ app1.js
@@ -3,3 +3,10 @@ console.log('hello world');
 console.log('hello america');
 
 console.log('second commit');
+
+console.log('third commit');
+function add() {
+    return 2 + 2;
+}
+
+console.log('third commit');
